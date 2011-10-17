50a439de4825863c5e914836a998a1a892764a42
Nick Lewis <nick@puppetlabs.com>
2011-10-17 14:34:50 -0700

    Support node yamldir as well as facts
    When the inventory service is enabled, we don't store facts in YAML,
    which means we don't have the data necessary for this script to work.
    Now, it can alternately be pointed at the node yamldir, and will produce
    similar results.
    
    Also added better error handling when there are no files, as well as
    when a file can't be read. In the latter case, we now simply report
    'unknown' for the issuer and timestamp, rather than failing the entire
    run outright.

--- bin/check_progress.rb
+++ bin/check_progress.rb
@@ -2,7 +2,7 @@ require 'date'
 require 'pathname'
 
 unless ARGV.length > 0
-  puts "usage: ruby check_progress.rb <fact_yaml_dir>"
+  puts "usage: ruby check_progress.rb $(puppet master --configprint yamldir)/node"
   exit 1
 end
 
@@ -10,29 +10,28 @@ dir = Pathname.new(ARGV.first)
 
 files = dir.entries.select { |f| f.extname == '.yaml' }
 
-if files.empty?
-  puts "#{dir} does not contain any YAML files"
-  exit 1
-end
+raise "#{dir} does not contain any YAML files" if files.empty?
 
 Dir.chdir(dir.to_s)
 
 groups = Hash.new { |h,k| h[k] = [] }
 
 files.each do |file|
-  yaml = file.read
+  name = file.basename('.yaml').to_s
+
+  # This will result in 'unknown' issuer and timestamp, if we can't read the
+  # file.
+  yaml = file.read rescue ''
 
   yaml =~ /^\s*agent_cert_on_disk_issuer: "?(.+?)"?\s*$/
   issuer = $1 || 'unknown'
 
-  yaml =~ /^\s*!ruby\/sym _timestamp: "?(.+)"?/
+  yaml =~ /!ruby\/sym _timestamp"?: "?(.+)"?/
   timestamp = $1 || 'unknown'
 
   # Remove the millisecond, etc bits. We just sub the string because parsing
   # and formatting the time is *phenomenally* slow (adds ~800% to the runtime).
   timestamp.sub!(/\..*/,'')
-
-  name = file.basename('.yaml').to_s
 
   groups[issuer] << [name, timestamp]
 end
