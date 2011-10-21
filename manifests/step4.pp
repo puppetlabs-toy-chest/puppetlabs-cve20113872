1cbcd029a5589721a9acd463e0cadcfb303ae1de
Jeff McCune <jeff@puppetlabs.com>
2011-10-21 09:42:52 -0700

    (#24) Fix unrecognized escape sequences
    Nigel points out in #24 the functions validating the values of the
    custom facts this module uses throw warnings into the system log.  These
    look like:
    
        warning: Unrecognised escape sequence '\.' in file
        /etc/puppetlabs/puppet/modules/cve20113872/manifests/init.pp at line 44
        warning: Unrecognised escape sequence '\.' in file
        /etc/puppetlabs/puppet/modules/cve20113872/manifests/init.pp at line 46
        warning: Unrecognised escape sequence '\.' in file
        /etc/puppetlabs/puppet/modules/cve20113872/manifests/init.pp at line 48
    
    The problem is that the code uses double quotes which causes the Puppet
    parser to perform string escaping before passing the string to the
    underlying function.
    
    This patch fixes the problem by converting the validation functions to
    string literals, thus avoiding the string escapes and allowing the
    underlying regular expression parser to handle the escape sequences
    properly.
    
    Closes #24

--- manifests/step4.pp
+++ manifests/step4.pp
@@ -32,7 +32,7 @@ class cve20113872::step4 {
   $module = "cve20113872"
 
   # The certificate has /CN=Puppet CA: $hostname
-  cve20113872_validate_re($agent_cert_on_disk_issuer, "^/CN=")
+  cve20113872_validate_re($agent_cert_on_disk_issuer, '^/CN=')
   # pe-puppet or puppet
   cve20113872_validate_re($agent_user, "puppet")
   # pe-puppet or puppet
@@ -42,11 +42,11 @@ class cve20113872::step4 {
   # /etc/puppetlabs/puppet/ssl/certs
   cve20113872_validate_re($agent_certdir, '^/')
   # /etc/puppetlabs/puppet/ssl/certs/ca.pem
-  cve20113872_validate_re($agent_localcacert, "^/.*?\.pem$")
+  cve20113872_validate_re($agent_localcacert, '^/.*?\.pem$')
   # /etc/puppetlabs/puppet/ssl/crl.pem
-  cve20113872_validate_re($agent_hostcrl, "^/.*?\.pem$")
+  cve20113872_validate_re($agent_hostcrl, '^/.*?\.pem$')
   # /var/run/pe-puppet/agent.pid
-  cve20113872_validate_re($agent_pidfile, "^/.*?\.pid$")
+  cve20113872_validate_re($agent_pidfile, '^/.*?\.pid$')
   # Certname can be anything, but it can't be empty.
   cve20113872_validate_re($agent_certname, ".")
   # Agents PID to reload it mid-run
